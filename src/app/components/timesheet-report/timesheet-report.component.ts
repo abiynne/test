eea88649198a12ef3299c14fe1898677a7c0de00
Shinto Chandy <shintochandy642@gmail.com>
2023-07-25 17:24:28 +0530

    View report page, download as excel and print pdf working fine

--- src/app/components/timesheet-report/timesheet-report.component.ts
+++ src/app/components/timesheet-report/timesheet-report.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 // Interface for TableRow (you can add more properties as needed)
 interface TableRow {
@@ -12,6 +15,7 @@ interface TableRow {
   templateUrl: './timesheet-report.component.html',
   styleUrls: ['./timesheet-report.component.css']
 })
+
 export class TimesheetReportComponent implements OnInit {
 
   // Assuming you have this variable with data for each day's hours
@@ -20,7 +24,7 @@ export class TimesheetReportComponent implements OnInit {
   // The displayed days will change based on the selected date range
   displayedDays: number[] = [];
   selectedDateRange: string = ''; // Default value
-
+  @ViewChild('table') table!: ElementRef;
   projects: string[] = [
     'Uprime',
     'Uprime R&D',
@@ -43,10 +47,9 @@ export class TimesheetReportComponent implements OnInit {
     const [startDate, endDate] = this.selectedDateRange.split(' to ');
     const [startDay, startMonth, startYear] = startDate.split('-');
     const [endDay, endMonth, endYear] = endDate.split('-');
-
     // Format the button text based on the selected date range
     const buttonText = `${startDay}-${startMonth}-${startYear} to ${endDay}-${endMonth}-${endYear}`;
-    
+
     // Update the button text
     this.selectedDateRange = buttonText;
   }
@@ -82,8 +85,52 @@ export class TimesheetReportComponent implements OnInit {
     return hours.reduce((acc, val) => acc + parseFloat(val), 0);
   }
 
+  exportToExcel(event: Event) {
+    // Prepare the data for the Excel file
+    const data: any[] = [];
+    const headerRow: any[] = [];
+    const rows: any[] = this.tableData;
+    const headers = this.displayedDays.map(day => `D${day}`);
+  
+    // Create the header row
+    headerRow.push('Project Name', ...headers, 'Total Hours');
+    data.push(headerRow);
+  
+    // Create data rows
+    for (const row of rows) {
+      const dataRow: any[] = [];
+      dataRow.push(row.project, ...row.hours, this.getTotalHours(row.hours));
+      data.push(dataRow);
+    }
+  
+    // Create the workbook and worksheet
+    const workbook: XLSX.WorkBook = { Sheets: {}, SheetNames: [] };
+    const worksheet: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
+  
+    // Add the worksheet to the workbook
+    workbook.SheetNames.push('Timesheet_Data');
+    workbook.Sheets['Timesheet_Data'] = worksheet;
+  
+    // Convert the workbook to an Excel file and download it
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'timesheet_data.xlsx';
+    a.click();
+    window.URL.revokeObjectURL(url);
+
+    event.preventDefault();
+  }
+
+printTableAsPdf(event: Event) {
+  window.print();
+  event.preventDefault();
+}
+  
   goToDashboard() {
     this.router.navigate(['/my-timesheets']);
   }
-  
+
 }
